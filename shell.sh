5c0c0a0251ce70a7f37102efea27bec33253363b
wing0110210317 <60053161+wing0110210317@users.noreply.github.com>
2021-08-24 20:40:05 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,7 +7,7 @@ echo "/ip firewall address-list remove [/ip firewall address-list find list=CN]"
 echo "/log info \"Remove finished!\""
 echo "/log info \"Start importing CN_IP_List!\""
 echo "/ip firewall address-list"
-echo "add list=CN address=192.168.0.0/16"
+echo "add list=CNIP address=192.168.0.0/16"
 nets=`cat ./chnroute.txt`
 for net in $nets ; do
   echo "add list=CNIP address=$net "

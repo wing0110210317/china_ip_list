69804de17a1620e097713ebafc007dc4dc65128a
wing0110210317 <60053161+wing0110210317@users.noreply.github.com>
2021-08-27 11:14:29 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,13 +1,8 @@
 {
 TZ="Asia/Shanghai" 
 echo -e "#Last Modified: $(date)"
-echo "/log info \"Start updating CN_IP_List!\""
-echo "/log info \"Start removing old list!\""
 echo "/ip firewall address-list remove [/ip firewall address-list find list=CNIP]"
-echo "/log info \"Remove finished!\""
-echo "/log info \"Start importing CN_IP_List!\""
 echo "/ip firewall address-list"
-echo "add list=CNIP address=192.168.0.0/16"
 nets=`cat ./chnroute.txt`
 for net in $nets ; do
   echo "add list=CNIP address=$net "
